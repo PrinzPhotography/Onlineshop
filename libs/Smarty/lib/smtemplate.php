e2a0e2ea6a4a244366c1d572856d1abfb94a6635
PrinzPhotography <134498451+PrinzPhotography@users.noreply.github.com>
2023-05-24 12:57:13 +0200

    Add files via upload

---
+++ libs/Smarty/lib/smtemplate.php
@@ -0,0 +1,29 @@
+/* Smarty Dateien laden */
+
+require_once('smarty/Smarty.class.php');
+require_once('smtemplate_config.php');
+
+
+
+
+
+
+
+class SMTemplate extends Smarty {
+
+
+
+    function _construct() {
+        parent::_construct();
+
+        global $smtemplate_config;
+        $this->template_dir = $smtemplate_config['template_dir'];
+        $this->compile_dir = $smtemplate_config['compile_dir'];
+        $this->cache_dir = $smtemplate_config['cache_dir'];
+        $this->config_dir = $smtemplate_config['config_dir'];
+    }
+}
+
+function render($template) {
+    $this->display($template . '.tpl');
+}
